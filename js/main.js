95c5a803df9003c692d0d3177509a53d8a2c1cee
baigny <baigny@gmail.com>
2018-11-06 21:11:31 +0530

    made changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 let restaurants,
   neighborhoods,
   cuisines
@@ -157,10 +159,10 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
-
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt=restaurant.name + "Restaurant";
   li.append(image);
 
   const name = document.createElement('h1');
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -89,7 +89,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-
+  image.alt =restaurnt.name + 'Restaurant';
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
 
