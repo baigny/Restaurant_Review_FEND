32132469138e8beb53f9fc2cdcb8b890aa8f77ba
baigny <baigny@gmail.com>
2018-11-07 10:48:14 +0530

    Responsive

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -112,7 +112,7 @@ fillRestaurantHoursHTML = (operatingHours = self.restaurant.operating_hours) => 
     const day = document.createElement('td');
     day.innerHTML = key;
     row.appendChild(day);
-
+    
     const time = document.createElement('td');
     time.innerHTML = operatingHours[key];
     row.appendChild(time);
